2976c3c12902d6d4c97e7f401ec7aef1d4fae1d9
Kostya <1_2_3_4_72@mail.ru>
2023-04-12 22:13:52 +0700

    Первое задание готово

---
+++ Langhw001/Program.cs
@@ -0,0 +1,14 @@
+﻿// Большее из двух чисел
+
+Console.WriteLine("Введите число a: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число b: ");
+int b = Convert.ToInt32(Console.ReadLine());
+if(a > b)
+{
+    Console.WriteLine("Большее число:" +a+ ",меньшее число:" +b);
+}
+else 
+{
+    Console.WriteLine("Большее число:" +b+ ",меньшее число:" +a);
+}
