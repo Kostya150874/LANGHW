ca5ed13fb1ffadc20faf5f8db96968143037fd24
Kostya <1_2_3_4_72@mail.ru>
2023-04-12 22:57:43 +0700

    Четвертое задание готово

---
+++ Langhw004/Program.cs
@@ -0,0 +1,10 @@
+﻿// Показать все четные от 1 до N
+
+Console.WriteLine("Введите число N:");
+int n = Convert.ToInt32(Console.ReadLine());
+
+for(int i=1;i<=n;i=i+1)
+{
+    if(i % 2 == 0)
+    Console.Write(i + ",");
+}
