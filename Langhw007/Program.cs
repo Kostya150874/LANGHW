15f2783bbcb08be05c21703c0b78073515efa350
Kostya <1_2_3_4_72@mail.ru>
2023-04-15 19:42:20 +0700

    Задача 15 про выходные

--- Langhw007/Program.cs
+++ Langhw007/Program.cs
@@ -5,8 +5,8 @@ int dayNumber = ReadInt("Введите число от 1 до 7: ");
 Console.WriteLine(WorkHoliday(dayNumber));
 int ReadInt(string message)
 {
-    Console.Write(message);
-    return Convert.ToInt32(Console.ReadLine());
+    Console.Write(message)
+    return Convert.ToInt32(Console.ReadLine())
 }
 string WorkHoliday(int a)
 {
