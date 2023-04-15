09b0456d101a161c9b2af8f5227816b4c30c2dec
Kostya <1_2_3_4_72@mail.ru>
2023-04-15 19:39:48 +0700

    Задача 13 про вывод третьей цифры числа

--- Langhw006/Program.cs
+++ Langhw006/Program.cs
@@ -1,28 +1,31 @@
 ﻿// Задача 13. Напишите программу, которая выводит третью цифру заданного 
 // числа или сообщает, что третьей цифры нет
 
-int Prompt(string msg)
+int number = ReadInt("Введите число: ");
+int count = number.ToString().Length;
+Console.Write(MakeArray(number, count));
+
+int ReadInt(string message)
 {
-    Console.WriteLine($"{msg}");
+    Console.Write(message);
     return Convert.ToInt32(Console.ReadLine());
 }
-int number = Prompt("Ведите число: ");
+int MakeArray(int a, int b)
+{
+int result = 0;
+    if (b < 3)
+    {
+        Console.Write("Третьей цифры нет, держи: ");
+    }
+    else
+    {
+        int c = 1;
+        for (int i = b; i > 3; i--)
+        {
+            c = c * 10;
+        }
 
-int fnumber(int number)
-{
-    while(number > 999)
-{
-    number /=10;
+        result = (a / c) % 10;
+    }
+return result;
 }
-    return number % 10;
-}
-bool check(int number);
-{
-    if (number < 100)
-    return false;
-    return true;
-}
-if (check(number) ! = true);
-Console.WriteLine("Третьей цифры нет");
-else
-Console.WriteLine($ "Третья цифра числа {number} является {fnumber(number)}");
